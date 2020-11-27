a3cd711e2a04afe7710294f1fcdd82ddaaa2ffd5
Thanos Kaloudis <38354865+Thanoskaloudis@users.noreply.github.com>
2020-11-27 13:03:22 +0000

    implement the logic for hadling the the format based on a url given

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,42 @@
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let formText = document.getElementById('url').value;
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    if(Client.checkForURL(formText)) {
+        console.log("::: Form Submitted :::")
+    
+        postData('http://localhost:8081/api', {url: formText})
+    
+        .then(function(res) {
+            document.getElementById("agreement").innerHTML = `Agreement: ${res.agreement}`;
+            document.getElementById("subjectivity").innerHTML = `Subjectivity: ${res.subjectivity}`;
+            document.getElementById("confidence").innerHTML = `Confidence: ${res.confidence}`;
+            document.getElementById("irony").innerHTML = `Irony: ${res.irony}`;
+        })
+        } else {
+            alert('Invalid URL, please try again');
+        }
 }
+
+const postData = async (url = "", data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        mode: 'cors',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+    try {
+        const newData = await response.json();
+        console.log('Responce received:', newData)
+        return newData;
+    } catch (error) {
+        console.log('error', error);
+    }
+};
 
 export { handleSubmit }
